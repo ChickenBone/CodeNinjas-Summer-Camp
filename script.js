ea96092cac5a26061ac3c11e16906f18670847d8
Wyatt Whorton <bluebomber019@gmail.com>
2019-03-03 22:53:38 -1000

    Added Functions to detect direction

--- script.js
+++ script.js
@@ -1,12 +1,63 @@
 function log(message){
-  setTimeout(function(){},100)
+  if(document.getElementById("dev").checked){
+  setTimeout(function(){},500)
   console.log(message)
   var p = document.getElementById("log")
   p.innerHTML += '<br>' + message;
   p.scrollTop = p.scrollHeight;
-
+  }
 }
+function spots(x, y) {
+  this.x = x;  
+  this.y = y;
+  this.arr = []
+  this.store = function (x,y) {
+
+  if(this.arr.length >= 5){
+      tempx = this.arr["4"].x
+      tempy = this.arr["4"].y
+      this.arr = []
+      this.arr.push({"x" : tempx, "y" : tempy});
+    }
+    this.arr.push({"x" : x, "y" : y});
+  }
+  this.up = function () {
+    try{
+    return this.arr["0"].y > this.arr["1"].y || this.arr["1"].y > this.arr["2"].y 
+    }catch{
+      log('[i] Array Reset')
+    }
+  }
+  this.down = function () {
+    try{
+
+    return this.arr["0"].y < this.arr["1"].y || this.arr["1"].y < this.arr["2"].y 
+  }catch{
+    log('[i] Array Reset')
+  }
+  }
+  this.left = function () {
+    try{
+
+    return this.arr["0"].x < this.arr["1"].x || this.arr["1"].x < this.arr["2"].x
+  }catch{
+    log('[i] Array Reset')
+  }
+  }
+  this.right = function () {
+    try{
+    return this.arr["0"].x > this.arr["1"].x || this.arr["1"].x > this.arr["2"].x
+  }catch{
+    log('[i] Array Reset')
+  }
+  }
+}
+
+const s = new spots;
 window.addEventListener("load", function(e) {
+  var p = document.getElementById("log")
+  p.innerHTML = ""
+  p.scrollTop = p.scrollHeight;
   var color = {r: 240, g: 100, b: 100};
   var slider = document.getElementById("tolerance");
   var canvas  = document.getElementById('canvas');
@@ -50,9 +101,25 @@ function getColorAt(webcam, x, y) {
   var pixel = context.getImageData(x, y, 1, 1).data;
   return {r: pixel[0], g: pixel[1], b: pixel[2]};
 }
+i = 0
 function drawRect(rect, context, color) {
   context.strokeStyle = "rgb(" + color.r + ", " + color.g + ", " + color.b + ")";
   context.fillRect((rect.x+(rect.width)/2),(rect.y+(rect.height)/2),6,6);
   context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-  log("x: "+rect.x+ " y: "+rect.y)
+  s.store(rect.x+(rect.width)/2, rect.y+(rect.height)/2)
+  i++
+  if(i > 2){
+    if(s.up()){
+      log("[i] Moving Up")
+    }
+    if(s.down()){
+      log("[i] Moving Down")
+    }
+    if(s.left()){
+      log("[i] Moving Left")
+    }
+    if(s.right()){
+      log("[i] Moving Right")
+    }
+  }
 }
